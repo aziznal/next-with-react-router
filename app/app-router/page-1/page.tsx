da14bf0bf1586affad35be555c906d43b629ca43
aziznal <aziznal.dev@gmail.com>
2025-04-21 08:21:28 +0300

    add sleep to app-router example

--- app/app-router/page-1/page.tsx
+++ app/app-router/page-1/page.tsx
@@ -1,6 +1,11 @@
+import { sleep } from "@/app/lib/sleep";
 import { NextNav } from "@/components/NextNav";
 
-export default function Page() {
+export const dynamic = "force-dynamic";
+
+export default async function Page() {
+  await sleep(3);
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <NextNav />
--- app/app-router/page.tsx
+++ app/app-router/page.tsx
@@ -1,6 +1,11 @@
 import { NextNav } from "@/components/NextNav";
+import { sleep } from "../lib/sleep";
 
-export default function WelcomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function WelcomePage() {
+  await sleep(3);
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <NextNav />
